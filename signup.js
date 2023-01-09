29ccaad1f2050c9f47c2dd545d5be90462d83364
Sanggyu Park <parksg5687@naver.com>
2023-01-10 02:22:47 +0900

    signup 1 commit

---
+++ signup.js
@@ -0,0 +1,12 @@
+// var student = document.querySelector('#student');
+// student.onclick = () => {
+//     student.style.color = '#1E00D3';
+
+//     document.querySelector('#teacher').style.color='#B7B7B7'
+// }
+
+// var teacher = document.querySelector('#teacher');
+// teacher.onclick = () => {
+//     teacher.style.color = '#1E00D3';
+//     document.querySelector('#student').style.color='#B7B7B7'
+// }
